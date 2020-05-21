a07685bea7912aebf485ba0c1e2a0f91276ccfda
Lauren Frankovich <frankovister@gmail.com>
2020-05-21 19:54:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,17 +8,18 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  push num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  push num1 * num2
 end
+multiplication
 
 def modulo(num1, num2)
-
+  push num1 % num2
 end
 
 def square_root(num)
-
+  push 
 end
