3a9b9ab512a8eff4d0be86f5016aca4574585489
Lauren Frankovich <frankovister@gmail.com>
2020-05-21 20:02:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,24 +6,24 @@ addition(num1, num2)
 def subtraction(num1, num2)
   num1 - num2
 end
-subtraction
+subtraction(num1, num2)
 
 def division(num1, num2)
   num1 / num2
 end
-division
+division(num1, num2)
 
 def multiplication(num1, num2)
   num1 * num2
 end
-multiplication
+multiplication(num1, num2)
 
 def modulo(num1, num2)
   num1 % num2
 end
-modulo
+modulo(num1, num2)
 
 def square_root(num)
   Math.sqrt(num)
 end
-square_root
+square_root(num)
